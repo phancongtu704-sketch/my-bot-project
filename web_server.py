1483c8c854d9e8bc3deec98c00e62b3a87976860
phancongtu704-sketch <phancongtu704@gmail.com>
2025-11-17 18:37:08 +0700

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -541,4 +541,10 @@ def run_flask():
     discord_thread.start()
     
     # Bật Flask Web Server trong luồng chính
-    print("Web S
+    print("Web Server đã khởi động trên 0.0.0.0:5000")
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000), debug=False)
+
+
+if __name__ == '__main__':
+    run_flask()
+    
