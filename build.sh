fd504621767a3121a13b66480577a3c24e4ee3da
phancongtu704-sketch <phancongtu704@gmail.com>
2025-11-22 15:12:40 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
-#!/usr/bin/env bash
-
-# Cài đặt các thư viện hệ thống cần thiết cho Pillow và các gói khác
-apt-get update
-apt-get install -y \
-  libglib2.0-0 \
-  libcairo2 \
-  libpango-1.0-0 \
-  libpangocairo-1.0-0 \
-  libgdk-pixbuf-2.0-0 \
-  libffi-dev \
-  shared-mime-info
+# Cài đặt các thư viện hệ thống cần thiết cho Pillow
+sudo apt-get update
+sudo apt-get install -y libffi-dev libssl-dev libjpeg-dev zlib1g-dev
 
 # Cài đặt các thư viện Python
 pip install -r requirements.txt
