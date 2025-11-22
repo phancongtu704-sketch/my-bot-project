afd2dcd84ee7ab677a48d86da236c1da9a7600c1
phancongtu704-sketch <phancongtu704@gmail.com>
2025-11-22 15:16:19 +0700

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-# Cài đặt các thư viện hệ thống cần thiết cho Pillow
-sudo apt-get update
-sudo apt-get install -y libffi-dev libssl-dev libjpeg-dev zlib1g-dev
-
-# Cài đặt các thư viện Python
-pip install -r requirements.txt
